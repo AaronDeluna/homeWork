6899a15b798da44bb0b4477b89dc9b44974bcf63
ivanmilovanov <aaron_deluna@mail.ru>
2024-08-16 10:57:28 +0300

    "Вынес передаваемые свойства в константы в классах iPhoneFactory и SamsungFactory"

--- src/homework3/phone/factory/factory/IphoneFactory.java
+++ src/homework3/phone/factory/factory/IphoneFactory.java
@@ -6,11 +6,16 @@ import homework3.phone.factory.components.Processor;
 import homework3.phone.factory.phone.Iphone;
 
 public class IphoneFactory {
+    private static final int MHZ = 1000;
+    private static final int MEGAPIXELS = 8;
+    private static final int WIDTH = 60;
+    private static final int HEIGHT = 200;
+    private static final int LENGTH = 10;
 
     public static Iphone createIphone() {
-        Processor[] processors = {new Processor(1000), new Processor(1000)};
-        Camera camera = new Camera(8);
-        Frame frame = new Frame(60, 200, 10);
+        Processor[] processors = {new Processor(MHZ), new Processor(MHZ)};
+        Camera camera = new Camera(MEGAPIXELS);
+        Frame frame = new Frame(WIDTH, HEIGHT, LENGTH);
         return new Iphone(processors, camera, frame);
     }
 }
--- src/homework3/phone/factory/factory/SamsungFactory.java
+++ src/homework3/phone/factory/factory/SamsungFactory.java
@@ -6,11 +6,16 @@ import homework3.phone.factory.components.Processor;
 import homework3.phone.factory.phone.Samsung;
 
 public class SamsungFactory {
+    private static final int MHZ = 1500;
+    private static final int MEGAPIXELS = 16;
+    private static final int WIDTH = 60;
+    private static final int HEIGHT = 200;
+    private static final int LENGTH = 15;
 
     public static Samsung createSamsung() {
-        Processor processor = new Processor(1500);
-        Camera camera = new Camera(16);
-        Frame frame = new Frame(60, 200, 15);
+        Processor processor = new Processor(MHZ);
+        Camera camera = new Camera(MEGAPIXELS);
+        Frame frame = new Frame(WIDTH, HEIGHT, LENGTH);
         return new Samsung(processor, camera, frame);
     }
 }
